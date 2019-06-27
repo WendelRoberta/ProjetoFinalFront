be231ed9dfb77e4e4796551046020ee9f4210b05
Wendel Roberta <wendel.roberta1995@gmail.com>
2019-06-27 20:28:31 -0300

    mudanças na nav bar

--- AppCrud/src/app/home/home.module.ts
+++ AppCrud/src/app/home/home.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderModule, FooterModule } from '../shared';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
+    HeaderModule,
+    FooterModule
   ]
 })
 export class HomeModule { }
