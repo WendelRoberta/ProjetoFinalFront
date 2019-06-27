01663ca5187ecdb1adb5b3bc6ea260800bf9662c
Wendel Roberta <wendel.roberta1995@gmail.com>
2019-06-27 14:35:45 -0300

    commit inicial

---
+++ AppCrud/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderModule } from './shared/modules/header/header.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    AppRoutingModule,
+    HeaderModule,
+    NgbModule.forRoot()
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AppCrud/src/app/shared/modules/header/header.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header.component';
+import { RouterModule } from '@angular/router';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NgbDropdownModule
+  ],
+  exports: [
+    HeaderComponent,
+    CommonModule
+  ],
+
+})
+export class HeaderModule { }
